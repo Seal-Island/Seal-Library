1a144f51172edd53ad6d8add16065b8478e4e3e3
Focamacho <renancolombomail@gmail.com>
2021-03-13 08:57:38 -0300

    Adicionar suporte pra NBT no SealStack.get(string)

--- seallibrary-sponge/src/main/java/com/focamacho/seallibrary/util/ItemStackUtilsSponge.java
+++ seallibrary-sponge/src/main/java/com/focamacho/seallibrary/util/ItemStackUtilsSponge.java
@@ -1,5 +1,7 @@
 package com.focamacho.seallibrary.util;
 
+import com.focamacho.seallibrary.forge.ForgeUtils;
+import net.minecraft.nbt.JsonToNBT;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.data.DataQuery;
 import org.spongepowered.api.item.ItemType;
@@ -7,11 +9,29 @@ import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.ItemStackSnapshot;
 
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 @SuppressWarnings("unused")
 public class ItemStackUtilsSponge {
 
     public static ItemStack getStackFromID(String id) {
+        String data = "";
+
+        if(id.contains(".withTag(")) {
+            String[] item = id.split(Pattern.quote(".withTag("));
+
+            if(item.length > 1) {
+                StringBuilder dataB = new StringBuilder();
+                for(int i = 1; i < item.length; i++) {
+                    dataB.append(item[i]);
+                }
+                data = dataB.toString();
+                data = data.substring(0, data.length() - 1);
+            }
+
+            id = item[0];
+        }
+
         String[] split = id.split(":");
         if(split.length == 3) {
             Optional<ItemType> type = Sponge.getRegistry().getType(ItemType.class, split[0] + ":" + split[1]);
@@ -25,11 +45,29 @@ public class ItemStackUtilsSponge {
                 return ItemStackSnapshot.NONE.createStack();
             }
 
-            return ItemStack.builder().fromContainer(type.get().getTemplate().toContainer().set(DataQuery.of("UnsafeDamage"), value)).build();
+            ItemStack stack = ItemStack.builder().fromContainer(type.get().getTemplate().toContainer().set(DataQuery.of("UnsafeDamage"), value)).build();
+
+            if(!data.isEmpty()) {
+                stack = setData(stack, data);
+            }
+
+            return stack;
         } else if(split.length == 2) {
             Optional<ItemType> type = Sponge.getRegistry().getType(ItemType.class, split[0] + ":" + split[1]);
-            return type.map(itemType -> ItemStack.builder().itemType(itemType).build()).orElseGet(ItemStackSnapshot.NONE::createStack);
+            ItemStack stack = type.map(itemType -> ItemStack.builder().itemType(itemType).build()).orElseGet(ItemStackSnapshot.NONE::createStack);
+            if(!data.isEmpty()) {
+                stack = setData(stack, data);
+            }
+            return stack;
         } else return ItemStackSnapshot.NONE.createStack();
+    }
+
+    private static ItemStack setData(ItemStack stack, String data) {
+        net.minecraft.item.ItemStack item = (net.minecraft.item.ItemStack) ForgeUtils.getForgeStack(stack);
+        try {
+            item.setTagCompound(JsonToNBT.getTagFromJson(data));
+        } catch(Exception ignored) {}
+        return (ItemStack) ForgeUtils.getServerStack(item).toOriginal();
     }
 
 }
