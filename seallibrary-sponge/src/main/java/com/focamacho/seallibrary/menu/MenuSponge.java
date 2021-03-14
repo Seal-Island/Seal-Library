2a4691f2083102eb90eec108646dd112a954b0c1
Focamacho <renancolombomail@gmail.com>
2021-03-14 20:49:19 -0300

    Verificar se o item não é null no menu

--- seallibrary-sponge/src/main/java/com/focamacho/seallibrary/menu/MenuSponge.java
+++ seallibrary-sponge/src/main/java/com/focamacho/seallibrary/menu/MenuSponge.java
@@ -118,7 +118,7 @@ public class MenuSponge extends Menu {
                 int finalIndex = index;
                 Task.builder().execute(() -> {
                     IMenuItem item = items.get(finalIndex);
-                    if (!slot.peek().isPresent() || !slot.peek().get().equals(item.getItem().toOriginal()))
+                    if (item.getItem() != null && !slot.peek().isPresent() || !slot.peek().get().equals(item.getItem().toOriginal()))
                         slot.set((ItemStack) item.getItem().toOriginal());
                 }).submit(SealLibrarySponge.instance);
             } else {
